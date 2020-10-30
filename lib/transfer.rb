6bf9e8c715f76f5e29990a9a4c14d15de952b549
mixinspace <islammadkaur@gmail.com>
2020-10-30 00:22:43 -0500

    commit tree 11cadb5902e19799b35b449b0191bfff6912f5c2

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
   # your code here
+    attr_reader :sender, :receiver
+    attr_accessor :balance, :status, :amount
+
+    @@trans = []
+
+    def initialize(sender, receiver, status="pending", amount)
+        @sender = sender
+        @receiver = receiver
+        @status = status
+        @amount = amount
+        @@trans << self
+    end
+
+    def valid?
+      sender.valid? && receiver.valid?
+    end
+
+    def execute_transaction
+      if sender.balance > amount && self.status == "pending"
+        sender.balance -= amount
+      end
+    end
 end
+ 
